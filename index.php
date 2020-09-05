2997f5d84f0b480732862b56dd1f5681a4f1d2c4
H. M Mahmudul Hasan <hhasan152041@bscse.uiu.ac.bd>
2020-09-05 13:35:03 +0600

    BetweenTheCovers PSD to Responsive

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang=" str_replace('_', '-', app()->getLocale()) ">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Between the Covers</title>
+
+        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Roboto:300,400,700">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        
+        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="css/responsive.css">
+
+    </head>
+    <body>
+        <header>
+            <div class="top-nav container">
+                <div class="logo">Between the Covers</div>
+                <ul>
+                    <li>
+                        <a href="#">Library</a>
+                        <a href="#">About</a>
+                        <a href="#">Blog</a>
+                        <a href="#">Book Request</a>
+                    </li>
+                </ul>
+            </div> 
+            <div class="hero container">
+                <div class="hero-copy">
+                    <h1>CSS Grid Example</h1>
+                    <p>A practical example of CSS grid for typical website layouts</p>
+                    <div class="hero-buttons">
+                        <a href="#" class="button button-white">Button 1</a>
+                        <a href="#" class="button button-white">Button 2</a>
+                    </div>
+                </div> 
+                <div class="hero-image">
+                    <img src="img/bookcover.png" alt="hero image">
+                </div>
+            </div> 
+        </header>
+        <div class="featured-section">
+            <div class="container">
+                    <h1 class="text-center">CSS Grid Example</h1>
+                    <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur ipsa molestias tenetur. Animi asperiores aspernatur atque labore nihil optio quod sequi vitae? Blanditiis dolorem itaque nobis officia sequi temporibus unde.</p>
+                    <div class="text-center button-container">
+                        <a href="#" class="button">Featured</a>
+                        <a href="#" class="button">Top Rated</a>
+                    </div>
+
+                <div class="products text-center">
+                    <div class="product" id="product1">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                    <div class="product" id="product2">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                    <div class="product" id="product3">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                    <div class="product" id="product4">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                    <div class="product" id="product5">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                    <div class="product" id="product6">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                    <div class="product" id="product7">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                    <div class="product" id="product8">
+                        <a href="#"><img src="img/book1.png" alt="product"></a>
+                        <a href="#"><span class="product name">Book Name 1</span></a>
+                        <div class="product-price">Rating: 4.5/5</div>
+                    </div>
+                </div> 
+                <div class="text-center button-container">
+                    <a href="#" class="button">View More Books</a>
+                </div>
+            </div> 
+        </div> 
+        <div class="blog-section">
+            <div class="container">
+                <h1 class="text-center">From Our Blog</h1>
+                <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores commodi error esse harum ipsam laboriosam libero maiores nobis perferendis praesentium quaerat qui rerum tempora, temporibus veritatis. Facilis quidem quod saepe!</p>
+                <div class="blog-posts">
+                    <div class="blog-post" id="blog1">
+                            <a href="#"><img src="img/blog1.png" alt="blog image"></a>
+                            <a href="#"><h2 class="blog-title">Blog Post Title</h2></a>
+                            <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, voluptatibus.</div>
+                    </div>
+                    <div class="blog-post" id="blog2">
+                        <a href="#"><img src="img/blog2.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Blog Post Title</h2></a>
+                        <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, voluptatibus.</div>
+                    </div>
+                    <div class="blog-post" id="blog3">
+                        <a href="#"><img src="img/blog3.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Blog Post Title</h2></a>
+                        <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, voluptatibus.</div>
+                    </div>
+                </div>
+            </div> 
+        </div> 
+        <footer>
+            <div class="footer-content container">
+                <div class="made-with">Made With <i class="fa fa-heart"></i> by H. M Mahmudul Hasan</div>
+                <ul>
+                    <li>Follow Me:
+                        <a href="#"><i class="fa fa-globe"></i></a>
+                        <a href="#"><i class="fa fa-youtube"></i></a>
+                        <a href="#"><i class="fa fa-github"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </footer> 
+    </body>
+</html>
